ed73806a0bcf281d233e83f778034664ffc36338
Dickson <dickson@rametech.com>
2026-03-22 15:00:28 -0800

    Fix: Convert ESLint config to flat format (v9+)
    Replace deprecated .eslintrc.json with eslint.config.js for ESLint 9.x compatibility

---
+++ backend/eslint.config.js
@@ -0,0 +1,33 @@
+const eslint = require('eslint');
+
+module.exports = [
+  {
+    files: ['src/**/*.js'],
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: 'module',
+      globals: {
+        console: 'readonly',
+        process: 'readonly',
+        require: 'readonly',
+        module: 'readonly',
+        __dirname: 'readonly',
+      },
+    },
+    rules: {
+      'indent': ['error', 2],
+      'linebreak-style': ['error', 'unix'],
+      'quotes': ['error', 'single'],
+      'semi': ['error', 'always'],
+      'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      'no-console': 'warn',
+      'no-var': 'error',
+      'prefer-const': 'error',
+      'eqeqeq': ['error', 'always'],
+      'curly': ['error', 'all'],
+      'brace-style': ['error', '1tbs'],
+      'comma-dangle': ['error', 'never'],
+      'arrow-spacing': ['error', { before: true, after: true }],
+    },
+  },
+];
